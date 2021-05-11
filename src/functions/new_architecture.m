6ae5ed9d5cb2414bcfdc5594df5a808610e46d67
frcon <frcon@LAPTOP-85A61LB1.homenet.telecomitalia.it>
2021-05-11 18:05:48 +0200

    Added multiple network testing

--- src/functions/new_architecture.m
+++ src/functions/new_architecture.m
@@ -1,36 +1,73 @@
 function layers = new_architecture(inputSize, version)
 
-    if version == 1
+    if version == 1 %8
 
-    layers = [
+        layers = [
         imageInputLayer(inputSize)
 
-        convolution2dLayer(3,32,'Padding','same')
-        leakyReluLayer(0.1)
+        convolution2dLayer(3,32)
+        reluLayer
 
         maxPooling2dLayer(2,'Stride',2)
 
-        convolution2dLayer(3,64,'Padding','same')
-        leakyReluLayer(0.1)
+        convolution2dLayer(3,64)
+        reluLayer
 
         maxPooling2dLayer(2,'Stride',2)
 
-        convolution2dLayer(3,128,'Padding','same')
-        leakyReluLayer(0.1)
+        convolution2dLayer(3,128)
+        reluLayer
         
         maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(3,256)
+        reluLayer
         
-        convolution2dLayer(3,256,'Padding','same')
-        leakyReluLayer(0.1)
+        fullyConnectedLayer(1024)
+        reluLayer
         
-        fullyConnectedLayer(256)
-        leakyReluLayer(0.1)
+        fullyConnectedLayer(1024)
+        reluLayer
+
         fullyConnectedLayer(4)
         softmaxLayer
         classificationLayer];
 
-    elseif version == 2
+    elseif version == 2 %9
 
+        layers = [
+        imageInputLayer(inputSize)
+
+        convolution2dLayer(5,32)
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(5,64)
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(5,128)
+        reluLayer
+        
+        maxPooling2dLayer(2,'Stride',2)
+
+        convolution2dLayer(5,256)
+        reluLayer
+        
+        fullyConnectedLayer(1024)
+        reluLayer
+        
+        fullyConnectedLayer(1024)
+        reluLayer
+
+        fullyConnectedLayer(4)
+        softmaxLayer
+        classificationLayer];
+    
+    elseif version == 3 %11
+        
         layers = [
         imageInputLayer(inputSize)
 
